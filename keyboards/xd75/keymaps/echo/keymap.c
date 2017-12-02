efcda7abc3e152a9bee4b9cb95cee7f487cd1679
echo <echo01248@gmail.com>
2017-12-02 00:23:43 -0800

    -=[] in the middle

--- keyboards/xd75/keymaps/echo/keymap.c
+++ keyboards/xd75/keymaps/echo/keymap.c
@@ -24,11 +24,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QW] = { /* QWERTY */
-  { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,   KC_NO, KC_NO, KC_NO, KC_6,   KC_7,       KC_8,    KC_9,    KC_0,    KC_BSPC },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_NO, KC_NO, KC_NO, KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_QUOT },
-  { KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_NO, KC_NO, KC_NO, KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_ENT  },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_NO, KC_NO, KC_NO, KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  { MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_NO, KC_NO, KC_NO, KC_SPC, MO(_RAISE), KC_RGUI, KC_RALT, KC_RCTL, KC_BSLS },
+  { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,   KC_NO,   KC_NO, KC_NO,   KC_6,   KC_7,       KC_8,    KC_9,    KC_0,    KC_BSPC },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_NO,   KC_NO, KC_NO,     KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_QUOT },
+  { KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_MINS, KC_NO, KC_LBRC, KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_ENT  },
+  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_EQL,  KC_NO, KC_RBRC, KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
+  { MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_NO,   KC_NO, KC_NO,   KC_SPC, MO(_RAISE), KC_RGUI, KC_RALT, KC_RCTL, KC_BSLS },
  },
  
  [_LOWER] = { /* FUNCTION */ 
